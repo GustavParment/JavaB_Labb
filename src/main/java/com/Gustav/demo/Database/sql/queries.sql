d9840d120039800767dd76e1f8f0ca47b8e70ca8
GustavParment <gustav.parment1993@gmail.com>
2023-12-28 09:15:01 +0100

    ready for presentation

--- src/main/java/com/Gustav/demo/Database/sql/queries.sql
+++ src/main/java/com/Gustav/demo/Database/sql/queries.sql
@@ -1,10 +1,1 @@
-select
-    m.MonsterName,
-    f.DamageDonebyDefender,
-    p.ClassName,
-    f.DamageDoneByAttacker,
-    f.Result
-from fighthistory f
-join monster m on m.monster_id = f.monster_id
-join player p on p.player_id = f.player_id
-where p.player_id = 9;
+select * from player
--- target/classes/main/java/com/Gustav/demo/Database/sql/queries.sql
+++
@@ -1,4 +0,0 @@
-select *
-from player p
-join dungeonrun.fighthistory f on p.player_id = f.player_id
-where p.player_id = 1
